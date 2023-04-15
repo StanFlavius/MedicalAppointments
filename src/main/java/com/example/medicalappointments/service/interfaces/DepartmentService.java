2606efcdb30c156d357b97bac4144d4c63d49ca1
OANA <todirica.oanaadreea@gmail.com>
2023-04-15 22:52:45 +0300

    MA-51: As an admin, I want to create and edit a department, so that it can reflect the new changes on the market.

--- src/main/java/com/example/medicalappointments/service/interfaces/DepartmentService.java
+++ src/main/java/com/example/medicalappointments/service/interfaces/DepartmentService.java
@@ -3,8 +3,16 @@ package com.example.medicalappointments.service.interfaces;
 import com.example.medicalappointments.model.Department;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentService {
 
     List<Department> getAllDepartments();
+
+    Department getDepartmentById(Long id);
+
+    Optional<Department> getDepartmentByName(String departmentName);
+
+    Department saveDepartment(Department department);
+
 }
