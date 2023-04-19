d0cfea8ad9eb3b9f1c903d79cdb5752f29bd832d
StanFlavius <flaviusstefanstan@yahoo.com>
2023-04-19 23:43:36 +0300

    MA-11: As a patient, I want to view the details for each department, so that I can choose the procedure and the doctor I will make an appointment with.

--- src/test/java/com/example/medicalappointments/controller/DepartmentControllerTest.java
+++ src/test/java/com/example/medicalappointments/controller/DepartmentControllerTest.java
@@ -39,12 +39,29 @@ public class DepartmentControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "pacient_1", roles={"PATIENT"})
+    public void showDepartmentInfo_pacient_user_success() throws Exception {
+        mockMvc.perform(get("/departments/{1}", "1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("department_info"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+
+    @Test
     @WithAnonymousUser
     public void showDepartmentInfo_unauthenticated_user_success() throws Exception {
         mockMvc.perform(get("/departments/{1}", "1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("department_info"))
                 .andExpect(content().contentType("text/html;charset=UTF-8"));
+    }
+
+    @Test
+    @WithAnonymousUser
+    void getAllByUnauthenticatedUser_success() throws Exception {
+        mockMvc.perform(get("/departments")
+                .flashAttr("department", Collections.singletonList(createDepartment())))
+                .andExpect(status().isOk());
     }
 
     @Test
