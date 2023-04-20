e798fe780473e91aae87d5a471b44898b1aab172
OANA <todirica.oanaadreea@gmail.com>
2023-04-20 14:20:49 +0300

    MA-15: verify date for appointment

--- src/main/java/com/example/medicalappointments/repository/ConsultRepository.java
+++ src/main/java/com/example/medicalappointments/repository/ConsultRepository.java
@@ -1,9 +1,18 @@
 package com.example.medicalappointments.repository;
 
 import com.example.medicalappointments.model.Consult;
+import com.example.medicalappointments.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public interface ConsultRepository extends JpaRepository<Consult, Long> {
+
+    @Query(value = "SELECT c FROM Consult c where c.doctor = :doctor AND c.date BETWEEN :dateStart AND :dateEnd")
+    Optional<Consult> findConsultInTimeRange(@Param("dateStart") Date dateStart, @Param("dateEnd") Date dateEnd, @Param("doctor") Doctor doctor);
 }
--- src/main/java/com/example/medicalappointments/service/ConsultService.java
+++ src/main/java/com/example/medicalappointments/service/ConsultService.java
@@ -1,12 +1,15 @@
 package com.example.medicalappointments.service;
 
 import com.example.medicalappointments.exception.CustomException;
+import com.example.medicalappointments.exception.EntityNotFoundException;
 import com.example.medicalappointments.model.Consult;
 import com.example.medicalappointments.model.Patient;
 import com.example.medicalappointments.repository.ConsultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 @Service
@@ -21,9 +24,40 @@ public class ConsultService {
         if (consult.getDate().before(new Date())) {
             throw new CustomException("Date must be in the future!");
         }
+        validateConsult(consult);
         Patient patient = patientService.findByUserId(userService.getCurrentUser().getId());
         consult.setPatient(patient);
 
         return consultRepository.save(consult);
+    }
+
+    private Boolean validateConsult(Consult consult) {
+        Date date = consult.getDate();
+        Date start = setHour(date, 8);
+        Date end = setHour(date, 18);
+
+        if(date.before(start) || date.after(end)) {
+            throw new CustomException("The time must be in the working hours!");
+        }
+
+        if(consultRepository.findConsultInTimeRange(addHours(date, -1), addHours(date, 1), consult.getDoctor()).isPresent()) {
+            throw new CustomException("There is already an appointment at this time, please try something else.");
+        }
+
+        return true;
+    }
+
+    private Date addHours(Date date, int hours) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        return calendar.getTime();
+    }
+
+    private Date setHour(Date date, int hour) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        return calendar.getTime();
     }
 }
